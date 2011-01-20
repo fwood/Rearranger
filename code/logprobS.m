46a1dd1915b806b9b004e9f87d560856dc2061b6
EmpyrealBoy <kassmaster@gmail.com>
2011-01-20 15:29:19 -0500

    here...

--- code/logprobS.m
+++ code/logprobS.m
@@ -1,7 +1,5 @@
 function [p] = logprobS(S,params)    
     
-getMRFparams();
-
 alpha = params.alpha;
 beta = params.beta;
 gamma = params.gamma;
@@ -14,18 +12,26 @@ s2 = 0;
 s3 = 0;
 s4 = 0;
 
-for v=1:size(S,1)
-    for b=1:size(S,2)
-        s1 = s1 + S(v,b)*M(v,b);
-        if b > 1
-            s2 = s2 + (S(v,b)-S(v,b-1))^2;
-        end
-        s3 = s3 + S(v,b)^2;
-        s4 = s4 + S(v,b)*M_prime(v,b);
-    end
-end
 
-p = exp(-alpha*s1 - beta*s2 + gamma*s3 - delta*s4);
+s1 = s1 - alpha * sum(sum(S.*M));
+s2 = s2 - beta * sum(sum(diff(S, [], 2) .^2));
+s3 = s3 + gamma * sum(S(:).*S(:));
+s4 = s4 - delta * sum(sum(S.*M_prime));
+
+p = exp(s1+s2+s3+s4);
+
+% for v=1:size(S,1)
+%     for b=1:size(S,2)
+%         s1 = s1 + S(v,b)*M(v,b);
+%         if b > 1
+%             s2 = s2 + (S(v,b)-S(v,b-1))^2;
+%         end
+%         s3 = s3 + S(v,b)^2;
+%         s4 = s4 + S(v,b)*M_prime(v,b);
+%     end
+% end
+% 
+% p = exp(-alpha*s1 - beta*s2 + gamma*s3 - delta*s4);
 
 end
 
