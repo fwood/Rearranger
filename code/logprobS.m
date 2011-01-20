f84915aab726e10de73b03a2ec9bb0468c029af8
EmpyrealBoy <kassmaster@gmail.com>
2011-01-20 14:42:44 -0500

    newly-titled logprobS.m and logprobSdiff.m

--- code/logprobS.m
+++ code/logprobS.m
@@ -1,14 +1,13 @@
 function [p] = probS(S,params)    
     
-
-[M,M_prime]=A2M(midNotePotential);
-
 getMRFparams();
 
 alpha = params.alpha;
 beta = params.beta;
 gamma = params.gamma;
 delta = params.delta;
+M = params.M;
+M_prime = params.M_prime;
 
 s1 = 0;
 s2 = 0;
@@ -18,15 +17,15 @@ s4 = 0;
 for v=1:size(S,1)
     for b=1:size(S,2)
         s1 = s1 + S(v,b)*M(v,b);
-        s3 = s3 + S(v,b)^2;
-        s4 = s4 + S(v,b)*M_prime(v,b);
         if b > 1
             s2 = s2 + (S(v,b)-S(v,b-1))^2;
         end
+        s3 = s3 + S(v,b)^2;
+        s4 = s4 + S(v,b)*M_prime(v,b);
     end
 end
 
-p = exp((-1) * alpha*s1 - beta*s2 + gamma*s3 - delta*s4);
+p = exp(-alpha*s1 - beta*s2 + gamma*s3 - delta*s4);
 
 end
 
