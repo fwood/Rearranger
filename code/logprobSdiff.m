428e8b452e77aef17f6bf26a9d84fac3fd9e9766
EmpyrealBoy <kassmaster@gmail.com>
2011-01-21 00:26:48 -0500

    updated logprobSdiff.m file

--- code/logprobSdiff.m
+++ code/logprobSdiff.m
@@ -19,26 +19,13 @@ delta = params.delta;
 M = params.M;
 M_prime = params.M_prime;
 
-s_alpha = alpha*(M(inds).*S(inds) - (M(inds).*(1-S(inds))));
+S_new = S;
+S_new(inds) = 1-S(inds);
 
-s_b1 = 0;
-s_b2 = 0;
-s_b3 = 0;
-for i=1:L
-    if i<=245 % if in first column
-        s_b1 = s_b1 + beta*((S(inds+245)-S(inds))^2 - (S(inds)-(1.-S(inds-245)))^2);
-    elseif i>(L-245) % if in last column
-        s_b2 = s_b2 + beta*((S(inds+245)-S(inds))^2 - (S(inds)-(1.-S(inds-245)))^2);
-    else % otherwise, look to either side
-        s_b3 = s_b3 + beta*((S(inds+245)-S(inds))^2 + (S(inds)-S(inds-245))^2)...
-            - beta*((S(inds+245)-(1.-S(inds)))^2 + (S(inds)-(1.-S(inds-245)))^2);
-    end
-end
-
-s_beta = s_b1 + s_b2 + s_b3;
-s_gamma = gamma*(sum(S(inds)==1) - (sum(S(inds)==0)));
-s_delta = delta*(M_prime(inds).*S(inds) - (M_prime(inds).*(1-S(inds))));
+s_alpha = alpha*(sum((M(inds).*S(inds))) - sum((M(inds).*(1-S(inds)))));
+s_beta = beta*(sum(sum(diff(S,[],2).^2)) - sum(sum(diff(S_new,[],2).^2)));
+s_gamma = gamma*((sum(S(inds)==0)) - sum(S(inds)==1));
+s_delta = delta*(sum((M_prime(inds).*(1-S(inds))) - sum(M_prime(inds).*S(inds))));
 
 d = s_alpha + s_beta + s_gamma + s_delta;
-
 end
