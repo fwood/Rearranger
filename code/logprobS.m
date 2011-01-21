c5e7c19a3c01d9c91d336b526dae4adc09b55457
EmpyrealBoy <kassmaster@gmail.com>
2011-01-20 22:39:45 -0500

    logprobS.m set (forgot to push this afternoon)

--- code/logprobS.m
+++ code/logprobS.m
@@ -1,5 +1,6 @@
-function [p] = logprobS(S,params)    
-    
+function [p,s2] = logprobS(S,params)    
+% this is the function to determine the log prob for S given the params   
+
 alpha = params.alpha;
 beta = params.beta;
 gamma = params.gamma;
@@ -7,33 +8,11 @@ delta = params.delta;
 M = params.M;
 M_prime = params.M_prime;
 
-s1 = 0;
-s2 = 0;
-s3 = 0;
-s4 = 0;
+s1 = -alpha * sum(sum(S.*M));
+s2 = -beta * sum(sum(diff(S, [], 2) .^2));
+s3 = gamma * sum(S(:).*S(:));
+s4 = -delta * sum(sum(S.*M_prime));
 
-
-s1 = s1 - alpha * sum(sum(S.*M));
-s2 = s2 - beta * sum(sum(diff(S, [], 2) .^2));
-s3 = s3 + gamma * sum(S(:).*S(:));
-s4 = s4 - delta * sum(sum(S.*M_prime));
-
-p = exp(s1+s2+s3+s4);
-
-% for v=1:size(S,1)
-%     for b=1:size(S,2)
-%         s1 = s1 + S(v,b)*M(v,b);
-%         if b > 1
-%             s2 = s2 + (S(v,b)-S(v,b-1))^2;
-%         end
-%         s3 = s3 + S(v,b)^2;
-%         s4 = s4 + S(v,b)*M_prime(v,b);
-%     end
-% end
-% 
-% p = exp(-alpha*s1 - beta*s2 + gamma*s3 - delta*s4);
-
+p = (s1+s2+s3+s4);
 end
 
-% alpha = -1*alpha;
-% beta = -1*beta;
