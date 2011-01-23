ce577f499e48f43193f44d9f76dfc1f2db48469c
EmpyrealBoy <kassmaster@gmail.com>
2011-01-23 17:18:45 -0500

    modified getMRFparams.m

--- code/getMRFparams.m
+++ code/getMRFparams.m
@@ -1,4 +1,4 @@
-function [params] = getMRFparams(B)
+function [params] = getMRFparams()
 
 params.alpha = 5;
 params.beta = 2.5;
@@ -8,7 +8,7 @@ params.delta = 1;
 K = 5; % instrument / voice
 N = 12*4+1; % notes (+1 for rest)
 
-midNotePotential = repmat([ abs(linspace(-1,1,N-1)) params.delta]',K,B);
+midNotePotential = repmat([ abs(linspace(-1,1,N-1)) params.delta]',K,1);
 
 params.M = midNotePotential;
 params.M_prime = params.M;
@@ -17,10 +17,14 @@ params.M_prime = params.M;
 for i=1:size(midNotePotential,1)
     if mod(i,49) == 0
         params.M(i,:) = zeros(size(midNotePotential,2),1)';        
+        params.M_bin(i,:) = zeros(size(midNotePotential,2),1)';
     end
     if mod(i,49) ~= 0
         params.M_prime(i,:) = zeros(size(midNotePotential,2),1)';        
+        params.M_bin(i,:) = ones(size(midNotePotential,2),1)';
     end
 end
+
+
 
 %params.epsilon = 1;
